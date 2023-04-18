979b1bf59c07c9ef42c08386dd8c3d8c066dbe65
Junior Castro <junior@juniorcastro.com>
2023-04-18 16:12:27 +0900

    fixing css in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,14 +13,14 @@ export const Button: React.FC<ButtonProps> = ({ variant = 'primary', ...props })
   var design_tokens = require('../../design_tokens/tokens.json');
   _style.backgroundColor = design_tokens.core.blue[500].value;
   _style.width = '208px';
-  _style.color: '#ffffff';
-  _style.padding: '12px 24px';
-  _style.fontFamily: 'Helvetica';
-  _style.fontWeight: '700';
-  _style.borderRadius: '3em';
-  _style.display: 'inline-block';
-  _style.border: 0;
-  _style.width: '208px';
+  _style.color = '#ffffff';
+  _style.padding = '12px 24px';
+  _style.fontFamily = 'Helvetica';
+  _style.fontWeight = '700';
+  _style.borderRadius = '3em';
+  _style.display = 'inline-block';
+  _style.border = 0;
+  _style.width = '208px';
   return (
     <button style={_style}>{children}</button>
   )
