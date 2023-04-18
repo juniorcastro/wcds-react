9308cb400a4620fbb34669000bdaa9b8a4769606
Junior Castro <junior@juniorcastro.com>
2023-04-18 15:47:42 +0900

    update button css styles refactoring

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './button.css';
 
 export interface ButtonProps extends
   React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
@@ -9,30 +10,9 @@ export const Button: React.FC<ButtonProps> = ({ variant = 'primary', ...props })
   const {children, style} = props;
 
   const _style: React.CSSProperties = style || {};
-
-  switch(variant) {
-    case 'primary':
-      _style.backgroundColor = "#194fdb";
-      _style.color = '#fff';
-      break;
-    case 'secondary':
-      _style.backgroundColor = "#F8CB2E";
-      _style.color = '#000';
-      break;
-    case 'danger':
-      _style.backgroundColor = "#B22727";
-      _style.color = '#fff';
-      break;
-  }
-
-  _style.padding = '12px 24px';
-  _style.fontFamily = 'Helvetica';
-  _style.fontWeight = 700;
-  _style.borderRadius = '3em';
-  _style.display = 'inline-block';
-  _style.border = 0;
-
+  var design_tokens = require('../../design_tokens/tokens.json');
+  _style.backgroundColor = design_tokens.core.blue[500].value;
   return (
-    <button style={_style} {...props}>{children}</button>
+    <button className={'primary-button'}>{children}</button>
   )
 }
