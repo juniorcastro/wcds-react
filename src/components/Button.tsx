25ee5b9475e99209f71bb34fd41bbecf3bd5cc97
Junior Castro <junior@juniorcastro.com>
2023-04-18 16:10:42 +0900

    fixing css in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './button.css';
+// import './button.css';
 
 export interface ButtonProps extends
   React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
@@ -12,7 +12,16 @@ export const Button: React.FC<ButtonProps> = ({ variant = 'primary', ...props })
   const _style: React.CSSProperties = style || {};
   var design_tokens = require('../../design_tokens/tokens.json');
   _style.backgroundColor = design_tokens.core.blue[500].value;
+  _style.width = '208px';
+  _style.color: '#ffffff';
+  _style.padding: '12px 24px';
+  _style.fontFamily: 'Helvetica';
+  _style.fontWeight: '700';
+  _style.borderRadius: '3em';
+  _style.display: 'inline-block';
+  _style.border: 0;
+  _style.width: '208px';
   return (
-    <button className={'primary-button'}>{children}</button>
+    <button style={_style}>{children}</button>
   )
 }
